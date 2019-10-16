fea9dee8026cbe1d230f838a046b94ead1fa64c6
jolina04 <coronelmariajolina@gmail.com>
2019-10-17 01:14:20 +0800

    Added static admin dashboard.

--- src/components/Admin/KCard.vue
+++ src/components/Admin/KCard.vue
@@ -1,18 +1,25 @@
 <template>
     <q-card class="my-card">
-        <q-card-section>
+        <q-card-section v-if="content_padding">
             <slot name="section"></slot>
         </q-card-section>
+
+        <slot name="content" v-if="!content_padding"></slot>
     </q-card>
 </template>
 
 <script>
     export default
     {
-        name: "KCard"
+        name: "KCard",
+        props: {
+            content_padding: {type: Boolean, default: true}
+        }
     }
 </script>
 
 <style scoped>
-
+    .q-card {
+        border-radius: 20px;
+    }
 </style>
---
+++ src/pages/Admin/PADashboardSalesBreakdown.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="q-ma-lg">
+        <span class="text-subtitle1">Sales Breakdown</span>
+        <k-card class="shadow-1" :content_padding="false">
+            <span slot="content">
+            <q-list separator>
+                <q-item clickable v-for="item in $options.salesBreakdownItems">
+                    <q-item-section avatar>
+                      <q-icon color="primary" :name="item.icon" />
+                    </q-item-section>
+
+                    <q-item-section>{{item.label}}</q-item-section>
+
+                    <q-item-section side>
+                        <q-item-label class="text-primary">0.00000003 BTC</q-item-label>
+                        <q-item-label>PHP 1,500.00</q-item-label>
+                    </q-item-section>
+                </q-item>
+            </q-list>
+        </span>
+        </k-card>
+    </div>
+</template>
+
+<script>
+    import KCard from '../../components/Admin/KCard'
+
+    export default {
+        name: "PADashboardSalesBreakdown",
+        components: {KCard},
+        salesBreakdownItems: [
+            {label: 'Daily Sales'   , icon: 'fas fa-calendar-day'},
+            {label: 'Week;y Sales'  , icon: 'far fa-calendar-alt'},
+            {label: 'Monthly Sales' , icon: 'calendar_today'},
+            {label: 'Annual Sales'  , icon: 'fas fa-calendar'}
+        ]
+    }
+</script>
+
+<style scoped>
+
+</style>
