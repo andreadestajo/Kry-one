4e5775ea6dfca6cd66b83d167808fc0d8c7d558e
Guillermo Tabligan <guillermotabligan@gmail.com>
2019-10-17 23:09:26 +0800

    member dashboard 50%

--- src/pages/Member/PMDashboard.vue
+++ src/pages/Member/PMDashboard.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="dashboard">
         <div class="dashboard__header"></div>
+
+        <!-- NOBILITY -->
         <div class="dashboard__nobility member__card q-mt-md">
             <div class="group">
                 <div class="group-label">Your current nobility</div>
@@ -15,6 +17,29 @@
                 <q-btn flat class="action-button"><q-icon name="fa fa-arrow-up"></q-icon> &nbsp; Accelerate</q-btn>
             </div>
         </div>
+
+        <!-- BITCOIN -->
+        <div class="dashboard__wallet member__card q-mt-md">
+            <div class="value">0.00000028 BTC</div>
+            <div class="conversion">PHP 1,500.00 | USD 24.30 </div>
+            <div class="label">Bitcoin Wallet</div>
+            <div class="action">
+                <q-btn flat class="action-button"><q-icon name="send"></q-icon> &nbsp; Send</q-btn>
+                <q-btn flat class="action-button"><q-icon name="fa fa-qrcode"></q-icon> &nbsp; Receive</q-btn>
+            </div>
+        </div>
+
+        <!-- UNIQ -->
+        <div class="dashboard__wallet member__card q-mt-md">
+            <div class="value">0.00000028 UNIQ</div>
+            <div class="conversion">PHP 1,500.00 | USD 24.30 </div>
+            <div class="label">Uniq Wallet</div>
+            <div class="action">
+                <q-btn flat class="action-button"><q-icon name="send"></q-icon> &nbsp; Send</q-btn>
+                <q-btn flat class="action-button"><q-icon name="fa fa-shopping-cart"></q-icon> &nbsp; Buy Uniq</q-btn>
+            </div>
+        </div>
+
 
     </div>
 </template>
