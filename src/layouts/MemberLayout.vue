83519c2ff713265d5cdb967a28f1bb9f7c14d826
Guillermo Tabligan <guillermotabligan@gmail.com>
2019-10-17 22:14:23 +0800

    member dashboard 20%

--- src/layouts/MemberLayout.vue
+++ src/layouts/MemberLayout.vue
@@ -1,17 +1,72 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-      <router-view />
-  </q-layout>
+    <q-layout class="member" view="lHh Lpr lFf">
+        <!--START HEADER-->
+        <q-header>
+            <q-toolbar>
+                <q-btn flat dense round @click="leftDrawerOpen = !leftDrawerOpen" icon="menu" aria-label="Menu"/>
+                <q-toolbar-title class="text-center">
+                    KryptoOne™
+                </q-toolbar-title>
+
+                <q-btn flat round dense icon="notifications">
+                    <!-- <q-badge floating color="red">2</q-badge> -->
+                </q-btn>
+            </q-toolbar>
+        </q-header>
+        <!--END HEADER-->
+
+        <!--START DRAWER-->
+        <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-2">
+            <q-list bordered separator>
+                <q-item class="full-width column no-wrap justify-center items-center content-center q-pa-lg text-grey-7">
+                    <span class="q-pa-sm">
+                        <q-avatar size="120px">
+                            <img src="https://cdn.quasar.dev/img/avatar.png">
+                        </q-avatar>
+                    </span>
+                    <span class="text-weight-bold">ADMIN</span>
+                    <span>admin@krypto.com</span>
+                </q-item>
+
+                <q-item class="text-grey-7" clickable v-ripple :active="false" v-for="item in $options.navigations" :key="item.label">
+                    <q-item-section avatar>
+                        <q-icon :name="item.icon" ></q-icon>
+                    </q-item-section>
+                    <q-item-section>{{item.label}}</q-item-section>
+                    <q-item-section side><!--You can put badge here--></q-item-section>
+                </q-item>
+            </q-list>
+        </q-drawer>
+        <!--END DRAWER-->
+
+        <q-page-container>
+            <!--START PAGES-->
+            <router-view />
+            <!--END PAGES-->
+        </q-page-container>
+    </q-layout>
 </template>
 
 <script>
-  export default {
-    name: 'MemberLayout',
+import styles from './MemberLayout.scss';
 
-    data () {
-      return {
+export default
+{
+    name: 'AdminLayout',
+    data: () =>
+    ({
         leftDrawerOpen: false
-      }
-    }
-  }
+    }),
+    navigations:
+    [
+        {label: 'Dashboard'       , icon: 'dashboard'               , side: '', route: ''},
+        {label: 'Wallet'          , icon: 'account_balance_wallet ' , side: '', route: ''},
+        {label: 'User'            , icon: 'people'                  , side: '', route: ''},
+        {label: 'KYC Submits'     , icon: 'verified_user'           , side: '', route: ''},
+        {label: 'Cashout Request' , icon: 'account_balance'         , side: '', route: ''},
+        {label: 'Commissions'     , icon: 'fas fa-street-view'      , side: '', route: ''},
+        {label: 'Monarchy View'   , icon: 'fas fa-piggy-bank'       , side: '', route: ''},
+        {label: 'Logout'          , icon: 'logout'                  , side: '', route: ''},
+    ]
+}
 </script>
--- src/pages/Member/PMDashboard.vue
+++ src/pages/Member/PMDashboard.vue
@@ -1,11 +1,31 @@
 <template>
-  <P>This is a member dashboard!</P>
+    <div class="dashboard">
+        <div class="dashboard__header"></div>
+        <div class="dashboard__nobility member__card q-mt-md">
+            <div class="group">
+                <div class="group-label">Your current nobility</div>
+                <div class="group-value current">PLEDGER</div>
+            </div>
+            <div class="group">
+                <div class="group-label">Your next target</div>
+                <div class="group-value next">KNIGHT</div>
+            </div>
+            <div class="action">
+                <q-btn flat class="action-button">Nobility</q-btn>
+                <q-btn flat class="action-button">Accelerate</q-btn>
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <script>
-    export default {
-        name: "PMDashboard"
-    }
+import styles from './PMDashboard.scss';
+
+export default
+{
+    name: "PMDashboard"
+}
 </script>
 
 <style scoped>
