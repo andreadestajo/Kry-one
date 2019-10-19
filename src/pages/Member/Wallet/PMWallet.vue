2384899f86d6405b1d4b5e22b8e914fe44dcef29
Guillermo Tabligan <guillermotabligan@gmail.com>
2019-10-19 11:27:00 +0800

    Member - Wallet (20%)

--- src/pages/Member/Wallet/PMWallet.vue
+++ src/pages/Member/Wallet/PMWallet.vue
@@ -1,18 +1,37 @@
 <template>
-    <div class="page-name">
-        Wallet Page
+    <div class="wallet">
+  <div class="q-pa-md">
+    <div class="q-gutter-y-md" style="max-width: 600px">
+      <q-card>
+        <q-tabs v-model="tab" dense class="text-grey" active-color="primary" indicator-color="primary" align="justify" narrow-indicator>
+			<q-tab v-for="currency in $options.currency_options" :key="currency.key" :name="currency.key" :label="currency.label" />
+        </q-tabs>
+
+        <q-separator />
+
+        <q-tab-panels v-model="tab" animated>
+			<q-tab-panel v-for="currency in $options.currency_options" :key="currency.key" :name="currency.key">
+				<div class="text-h6">Mails</div>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit.
+			</q-tab-panel>
+        </q-tab-panels>
+      </q-card>
+    </div>
+  </div>
     </div>
 </template>
 
 <script>
+import styles from './PMWallet.scss';
+import ref_currencies from '../../../references/refs_currencies';
+
 export default
 {
     filters: { },
     data:() =>(
     {
+		tab: 'uniq',
     }),
-    mounted() { },
-    methods: { },
-    computed: { }
+	currency_options: ref_currencies,
 }
 </script>
---
+++ src/references/refs_currencies.js
@@ -0,0 +1,6 @@
+export default 
+[
+    { key: 'uniq', label: 'Uniq Wallet', currency: 'UNIQ' },
+    { key: 'btc', label: 'Bitcoin', currency: 'BTC' },
+    { key: 'eth', label: 'Ethereum', currency: 'ETH' },
+]
