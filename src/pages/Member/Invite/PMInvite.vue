09d0750aebbfb040ea967012b46087872ce9499a
Guillermo Tabligan <guillermotabligan@gmail.com>
2019-10-19 18:13:30 +0800

    invite page done

--- src/pages/Member/Invite/PMInvite.vue
+++ src/pages/Member/Invite/PMInvite.vue
@@ -1,18 +1,57 @@
 <template>
-    <div class="page-name">
-        INVITE
+    <div class="invite">
+        <div class="invite__image">
+            <q-img class="img" :ratio="5/2" src="../statics/invite.jpg"></q-img>
+        </div>
+        <div class="invite__info">
+            <div class="label">SHARE YOUR REFERRAL CODE</div>
+            <div class="code">
+                <div class="code-value">EFZPWM</div>
+            </div>
+            <div class="share">
+                <q-btn color="primary">Copy Share Link</q-btn>
+            </div>
+            <div class="how">
+                <div class="main">HOW IT WORKS</div>
+                <div class="list">
+                    <div v-for="step in $options.step_options" :key="step.step" class="list-container">
+                        <div class="list-title">
+                            <div class="number">{{ step.step }}</div>
+                            <div class="value">{{ step.title }}</div>
+                        </div>
+                        <div class="list-detail">
+                            {{ step.detail }}
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
     </div>
 </template>
 
 <script>
+import styles from './PMInvite.scss';
+import KHeader from '../../../components/Member/KHeader';
+import KCard from '../../../components/Member/KCard';
+
 export default
 {
+    components: {KHeader, KCard},
     filters: { },
     data:() =>(
     {
     }),
     mounted() { },
     methods: { },
-    computed: { }
+    computed: { },
+    step_options:
+    [
+        { step: 1, title: 'Share your referral code', detail: 'Share your referral link or sign up code to friends.' },
+        { step: 2, title: 'Help Friends Sign Up', detail: 'Share your referral link or sign up code to friends.' },
+        { step: 3, title: 'Get Rewards', detail: 'Share your referral link or sign up code to friends.' },
+    ],
 }
 </script>
