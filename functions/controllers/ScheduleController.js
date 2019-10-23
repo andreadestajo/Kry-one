ab5650601cd0547dff28120eafa7e8869410b8d9
Guillermo Tabligan <guillermotabligan@gmail.com>
2019-10-23 23:00:01 +0800

    Building Currency Conversion Logic DB

--- functions/controllers/ScheduleController.js
+++ functions/controllers/ScheduleController.js
@@ -8,8 +8,9 @@ module.exports =
 
         for(let key=0;key<based_currency.length;key++)
         {
-            let currency = based_currency[key];
-            let conversion = await module.exports.getConversion(currency);
+            let currency            = based_currency[key];
+            let conversion          = await module.exports.getConversion(currency);
+            
             await MDB_CURRENCY.update(currency, conversion);
         }
 
@@ -22,14 +23,11 @@ module.exports =
 
         let conversion_api_key = process.env.CONVERSION_KEY;
         let url = `https://min-api.cryptocompare.com/data/price?fsym=${currency}&tsyms=${conversion_currency.join()}&api_key=${conversion_api_key}`;
-        console.log(url);
 
         await axios.get(url).then(function (response)
         {
             res = response.data;
             
-        }).catch(function (error)
-        {
         });
         
         return res;
