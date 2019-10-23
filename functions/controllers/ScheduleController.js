7d5573c4ab4251c9dcb547e929f2f40b781fcbdc
Guillermo Tabligan <guillermotabligan@gmail.com>
2019-10-23 18:49:58 +0800

    Building Currency Conversion Logic DB

---
+++ functions/controllers/ScheduleController.js
@@ -0,0 +1,8 @@
+module.exports =
+{
+	updateCurrency (request, response)
+	{
+        console.log("hello world");
+        response.end("Currency Updated");
+	},
+};
--- functions/index.js
+++ functions/index.js
@@ -6,15 +6,19 @@ const FUNCTIONS = require('firebase-functions');
 
 const FUNCTIONS_REGION    = FUNCTIONS.region('asia-northeast1');
 const FUNCTIONS_HTTPS     = FUNCTIONS_REGION.https; // FUNCTIONS.https
+const FUNCTIONS_PUBSUB    = FUNCTIONS_REGION.pubsub; // FUNCTIONS.https
 const FUNCTIONS_FIRESTORE = FUNCTIONS_REGION.firestore; // For triggers
 
-const authentication_controller = require('./controllers/Authentication');
-const account_controller        = require('./controllers/AccountController');
+const authentication_controller     = require('./controllers/Authentication');
+const account_controller            = require('./controllers/AccountController');
+const schedule_controller           = require('./controllers/ScheduleController');
 
 //exports.login        = FUNCTIONS_HTTPS.onCall(authentication_controller.login);
 //exports.registration = FUNCTIONS_HTTPS.onCall(authentication_controller.registration);
 
 // Account
-exports.register      = FUNCTIONS_HTTPS.onCall(account_controller.register);
-exports.login         = FUNCTIONS_HTTPS.onCall(account_controller.login);
-exports.resetPassword = FUNCTIONS_HTTPS.onCall(account_controller.resetPassword);
+exports.register        = FUNCTIONS_HTTPS.onCall(account_controller.register);
+exports.login           = FUNCTIONS_HTTPS.onCall(account_controller.login);
+exports.resetPassword   = FUNCTIONS_HTTPS.onCall(account_controller.resetPassword);
+exports.updateCurrencyM = FUNCTIONS_HTTPS.onRequest(schedule_controller.updateCurrency);
+exports.updateCurrency  = FUNCTIONS_PUBSUB.schedule('every 5 minutes').onRun(schedule_controller.updateCurrency);
