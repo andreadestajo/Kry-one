d40fce2e0c90f825ef326e6ff750990d121b4b27
jolina04 <coronelmariajolina@gmail.com>
2019-10-27 14:11:04 +0800

    Added card to nobility form.

--- src/pages/Admin/Nobilities/PANobilitiesEditModal.vue
+++ src/pages/Admin/Nobilities/PANobilitiesEditModal.vue
@@ -1,69 +1,71 @@
 <template>
     <k-modal ref="kModalRef">
-        <span slot="modal-content">
-            <k-field label="Title">
-               <q-input dense
-                        outlined
-                        type="text"
-                        v-model="nobility.title"/>
-            </k-field>
+        <q-card slot="modal-content" style="width: 50vh">
+            <q-card-section>
+                <k-field label="Title">
+                    <q-input dense
+                             outlined
+                             type="text"
+                             v-model="nobility.title"/>
+                </k-field>
 
-            <k-field label="Price">
-               <q-input dense
-                        outlined
-                        type="number"
-                        v-model="nobility.price"/>
-            </k-field>
+                <k-field label="Price">
+                    <q-input dense
+                             outlined
+                             type="number"
+                             v-model="nobility.price"/>
+                </k-field>
 
-            <k-field label="Rank Order">
-               <q-input dense
-                        outlined
-                        type="number"
-                        v-model="nobility.rank_order"/>
-            </k-field>
+                <k-field label="Rank Order">
+                    <q-input dense
+                             outlined
+                             type="number"
+                             v-model="nobility.rank_order"/>
+                </k-field>
 
-            <k-field label="Direct">
-               <q-input dense
-                        outlined
-                        type="number"
-                        v-model="nobility.required_direct"/>
-            </k-field>
+                <k-field label="Direct">
+                    <q-input dense
+                             outlined
+                             type="number"
+                             v-model="nobility.required_direct"/>
+                </k-field>
 
-            <k-field label="Rank">
-               <q-input dense
-                        outlined
-                        type="text"
-                        v-model="nobility.required_rank"/>
-            </k-field>
+                <k-field label="Rank">
+                    <q-input dense
+                             outlined
+                             type="text"
+                             v-model="nobility.required_rank"/>
+                </k-field>
 
-            <k-field label="Bonus">
-               <q-input dense
-                        outlined
-                        type="number"
-                        v-model="nobility.override_bonus"/>
-            </k-field>
+                <k-field label="Bonus">
+                    <q-input dense
+                             outlined
+                             type="number"
+                             v-model="nobility.override_bonus"/>
+                </k-field>
 
-            <k-field label="Perks">
-               <q-input dense
-                        outlined
-                        type="text"
-                        v-model="nobility.perks"/>
-            </k-field>
+                <k-field label="Perks">
+                    <q-input dense
+                             outlined
+                             type="text"
+                             v-model="nobility.perks"/>
+                </k-field>
 
-            <k-field label="Details">
-               <q-input dense
-                        outlined
-                        type="textarea"
-                        v-model="nobility.details"/>
-            </k-field>
+                <k-field label="Details">
+                    <q-input dense
+                             outlined
+                             type="textarea"
+                             v-model="nobility.details"/>
+                </k-field>
 
-            <k-field label="Badge Color">
-        <q-input dense
-                 outlined
-                 type="text"
-                 v-model="nobility.badge_color"/>
-            </k-field>
-        </span>
+                <k-field label="Badge Color">
+                    <q-input dense
+                             outlined
+                             type="text"
+                             v-model="nobility.badge_color"/>
+                </k-field>
+            </q-card-section>
+        </q-card>
 
         <span slot="modal-footer">
             <q-btn flat label="Save" @click="confirmUpdateNobility()" />
