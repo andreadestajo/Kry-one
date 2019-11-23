bb77b7d58a0d3d112eaaf683bb5d0c0c8c3f68d9
Guillermo Tabligan <guillermotabligan@gmail.com>
2019-11-23 15:54:18 +0800

    nobilities

--- src/pages/Member/Nobilities/PMNobilitiesRoyalties.vue
+++ src/pages/Member/Nobilities/PMNobilitiesRoyalties.vue
@@ -1,6 +1,30 @@
 <template>
     <div class="royalties">
-        Royalties
+        <div class="royalties__list">
+            <div class="list">
+                <div class="list-badge">
+                    <div class="circle"></div>
+                </div>
+                <div class="list-info">
+                    <div class="name">Duke</div>
+                    <div class="detail">
+                        <span class="label q-mr-sm">Required direct:</span>  
+                        <span class="value"><b>10 Marquees</b></span>
+                    </div>
+                    <div class="detail">
+                        <div class="label">Royalties:</div>
+                        <div class="value"><b>23% + 1.00% + Stock Option</b></div>
+                    </div>
+                    <div class="description">
+                        - Entitled to have a share on 1% of total sales of the company.
+                        - Entitled to become part owner of the company via stock option
+                        - Can be CEO, Board of Director, or Investor of any group of companies.
+                        - 23.00% on personal sales
+                        - Override commission on total teams sales on each direct team.
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
